068b4884fc8ab120ab8ff931795081fbb625740c
Moises Lozano Acosta <moiseslozano95@gmail.com>
2026-02-18 10:35:39 -0600

    feat: agregar botones de PrimeNG (Success y Primary)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button'; // 1. Importamos el módulo
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Asegúrate de que tenga esto
+  imports: [RouterOutlet, ButtonModule], // 2. Agregamos ButtonModule aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
